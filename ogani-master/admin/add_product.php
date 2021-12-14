7ede7cb52ac94421a45d0de53a4cee5abb4fe260
Iqra Azam <iqra.azam.dev@gmail.com>
2021-12-14 16:50:41 +0500

    Commited

---
+++ ogani-master/admin/add_product.php
@@ -0,0 +1,124 @@
+<?php include 'includes/header.php' ?>
+            
+            
+<!-- nav -->
+ <?php include 'includes/nav.php' ?>
+    
+<div class="pcoded-content">
+                        <div class="pcoded-inner-content">
+
+                            <!-- Main-body start -->
+                            <div class="main-body">
+                                <div class="page-wrapper">
+									<!-- Page-header start -->
+                                    <div class="page-header card">
+                                        <div class="card-block">
+                                            <h5 class="m-b-10">Basic Form Inputs</h5>
+                                            <p class="text-muted m-b-10">lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+                                            <ul class="breadcrumb-title b-t-default p-t-10">
+                                                <li class="breadcrumb-item">
+                                                    <a href="index.html"> <i class="fa fa-home"></i> </a>
+                                                </li>
+                                               <li class="breadcrumb-item"><a href="#!">Basic Componenets</a>
+                                                        </li>
+                                                        <li class="breadcrumb-item"><a href="#!">Basic Form Inputs</a>
+                                                        </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="page-body">
+                                        <div class="row">
+                                            <div class="col-sm-12">
+                                                <!-- Basic Form Inputs card start -->
+                                                <div class="card">
+                                                    <div class="card-header">
+                                                        <h5>Basic Form Inputs</h5>
+                                                        <span>Add class of <code>.form-control</code> with <code>&lt;input&gt;</code> tag</span>
+                                                        <div class="card-header-right"><i
+                                                            class="icofont icofont-spinner-alt-5"></i></div>
+
+                                                            <div class="card-header-right">
+                                                                <i class="icofont icofont-spinner-alt-5"></i>
+                                                            </div>
+
+                                                        </div>
+                                                        <div class="card-block">
+                                                            <h4 class="sub-title">Basic Inputs</h4>
+                                                            <form>
+                                                                <div class="form-group row">
+                                                                    <label class="col-sm-2 col-form-label">Simple Input</label>
+                                                                    <div class="col-sm-10">
+                                                                        <input type="text" class="form-control">
+                                                                    </div>
+                                                                </div>
+                                                                <div class="form-group row">
+                                                                    <label class="col-sm-2 col-form-label">Placeholder</label>
+                                                                    <div class="col-sm-10">
+                                                                        <input type="text" class="form-control"
+                                                                        placeholder="Type your title in Placeholder">
+                                                                    </div>
+                                                                </div>
+                                                                <div class="form-group row">
+                                                                    <label class="col-sm-2 col-form-label">Password</label>
+                                                                    <div class="col-sm-10">
+                                                                        <input type="password" class="form-control"
+                                                                        placeholder="Password input">
+                                                                    </div>
+                                                                </div>
+                                                                <div class="form-group row">
+                                                                    <label class="col-sm-2 col-form-label">Read only</label>
+                                                                    <div class="col-sm-10">
+                                                                        <input type="text" class="form-control"
+                                                                        placeholder="You can't change me" readonly>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="form-group row">
+                                                                    <label class="col-sm-2 col-form-label">Disable Input</label>
+                                                                    <div class="col-sm-10">
+                                                                        <input type="text" class="form-control"
+                                                                        placeholder="Disabled text" disabled>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="form-group row">
+                                                                    <label class="col-sm-2 col-form-label">Predefine
+                                                                        Input</label>
+                                                                        <div class="col-sm-10">
+                                                                            <input type="text" class="form-control"
+                                                                            value="Enter yout content after me">
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="form-group row">
+                                                                        <label class="col-sm-2 col-form-label">Select Box</label>
+                                                                        <div class="col-sm-10">
+                                                                            <select name="select" class="form-control">
+                                                                                <option value="opt1">Select One Value Only</option>
+                                                                                <option value="opt2">Type 2</option>
+                                                                                <option value="opt3">Type 3</option>
+                                                                                <option value="opt4">Type 4</option>
+                                                                                <option value="opt5">Type 5</option>
+                                                                                <option value="opt6">Type 6</option>
+                                                                                <option value="opt7">Type 7</option>
+                                                                                <option value="opt8">Type 8</option>
+                                                                            </select>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="form-group row">
+                                                                        <label class="col-sm-2 col-form-label">Round Input</label>
+                                                                        <div class="col-sm-10">
+                                                                            <input type="text"
+                                                                            class="form-control form-control-round"
+                                                                            placeholder=".form-control-round">
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="form-group row">
+                                                                        <label class="col-sm-2 col-form-label">Maximum
+                                                                            Length</label>
+                                                                            <div class="col-sm-10">
+                                                                                <input type="text" class="form-control"
+                                                                                placeholder="Content must be in 6 characters"
+                                                                                maxlength="6">
+                                                                            </div>
+                                                                        </div>
+                                                                   
+      
+<?php include "includes/footer.php" ?>
