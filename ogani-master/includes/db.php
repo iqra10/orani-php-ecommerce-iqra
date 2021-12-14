f437384f7d9208a9c5dacde15a4b3899f19d8aa3
Iqra Azam <iqra.azam.dev@gmail.com>
2021-12-14 15:44:50 +0500

    Chnaged

---
+++ ogani-master/includes/db.php
@@ -0,0 +1,16 @@
+<?php 
+
+$db_host = 'localhost';
+$db_user = 'root';
+$db_password = '';
+$db_name = 'orani_php_ecommerce_iqra';
+
+$connection = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+
+//if($connection) {
+//    
+//  echo 'Connected';  
+//    
+//}
+
+?>
