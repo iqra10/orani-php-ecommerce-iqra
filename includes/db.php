371813bf065bb955fc912317a62b148605a79b1b
boospot <boospotmanager@gmail.com>
2021-12-14 20:34:28 +0500

    moved files from sub directory to main directory and fixed product grid

--- includes/db.php
+++ includes/db.php
@@ -2,8 +2,8 @@
 
 $db_host = 'localhost';
 $db_user = 'root';
-$db_password = '';
-$db_name = 'orani_php_ecommerce_iqra';
+$db_password = 'root';
+$db_name = 'orani-php-ecommerce-iqra';
 
 $connection = mysqli_connect($db_host, $db_user, $db_password, $db_name);
 
