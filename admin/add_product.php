c406b81a97048e0e572b6538bbd398c5cf395a91
boospot <boospotmanager@gmail.com>
2021-12-17 20:28:48 +0500

    product add query updated

--- admin/add_product.php
+++ admin/add_product.php
@@ -1,112 +1,114 @@
 <?php include 'includes/header.php' ?>
-            
-            
-<!-- nav -->
- <?php include 'includes/nav.php' ?>
-    
-<div class="pcoded-content">
-                        <div class="pcoded-inner-content">
-                                    <div class="page-body">
-                                        <div class="row">
-                                            <div class="col-sm-12">
-                                                <!-- Basic Form Inputs card start -->
-                                                <div class="card">
 
-                                                <div class="card-block">
-                                                    <h4>Add Product</h4>
-                                                    
+
+    <!-- nav -->
+<?php include 'includes/nav.php' ?>
+
+    <div class="pcoded-content">
+    <div class="pcoded-inner-content">
+    <div class="page-body">
+    <div class="row">
+    <div class="col-sm-12">
+    <!-- Basic Form Inputs card start -->
+    <div class="card">
+
+    <div class="card-block">
+    <h4>Add Product</h4>
+
 <?php
-    
- if (isset($_POST['add'])) {
-     
-        $title = $_POST['title'];
-        $price = $_POST['price'];
-        $id_dep = $_POST['category'];
-        $description = $_POST['description'];
-        $info = $_POST['info'];
-        $img = $_FILES['img']['name'];
-        $img_temp = $_FILES['img']['tmp_name'];
-		
-	move_uploaded_file($img_temp, "./../img/$img"); 
-     
-     
-    $query = "INSERT INTO products(title, img, price, description, info, dep_id)" ;
-     
-    $result = "VALUES('{$title}', '{$img}', '{$price}', '{$description}', '{$info}', '{$id_dep}') ";
-     
-    echo "<p style='color: #0A77F4; font-size: 16px; padding-top: 10px;'>Fruit Added.</p>";
 
-     
- } 
-                                                    
-?>                                                
-                                                    <form method="post" enctype="multipart/form-data">
-                                                        <div class="form-group row">
-                                                            <label class="col-sm-2 col-form-label">Product Name</label>
-                                                            <div class="col-sm-10">
-                                                                <input type="text" name="title" class="form-control">
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group row">
-                                                            <label class="col-sm-2 col-form-label">Price</label>
-                                                            <div class="col-sm-10">
-                                                                <input type="text" name="price" class="form-control">
-                                                            </div>
-                                                        </div>
+if ( isset( $_POST['add'] ) ) {
+    global $connection;
 
-                                                        <div class="form-group row">
-                                                            <label class="col-sm-2 col-form-label">Select Category</label>
-                                                            <div class="col-sm-10">
-                                                            <select name="category" class="form-control">
-                                                                
-  <?php 
-                        
-  $query = "SELECT * FROM departments" ;
-                        
-  $select_all_query = mysqli_query($connection, $query);
-                        
-  while ($row = mysqli_fetch_array($select_all_query)) {
-      
-      $id = $row['id'];
-      $title = $row['title'];
-      
-echo "<option value='$id'>$title</option>";      
- 
-  }                        
-                        
-        
-                        
-                        
-?>                                      
-                     
-                                                                   </select>            
-                                                                </div>
-                                                            </div>
+	$title       = $_POST['title'];
+	$price       = $_POST['price'];
+	$id_dep      = $_POST['category'];
+	$description = $_POST['description'];
+	$info        = $_POST['info'];
+	$img         = $_FILES['img']['name'];
+	$img_temp    = $_FILES['img']['tmp_name'];
 
-                                                                    <div class="form-group row">
-                                                                        <label class="col-sm-2 col-form-label">Description</label>
-                                                                        <div class="col-sm-10">
-                                                                        <textarea rows="5" cols="5" class="form-control" name="description"></textarea>
-                                                                        </div>
-                                                                    </div>
+	move_uploaded_file( $img_temp, "./../img/$img" );
 
-                                                          <div class="form-group row">
-                                                                        <label class="col-sm-2 col-form-label">Information</label>
-                                                                        <div class="col-sm-10">
-                                                                            <textarea rows="5" cols="5" name="info" class="form-control"></textarea>
-                                                                        </div>
-                                                                    </div>
-                                                                
-                                                                
-                                                                 <div class="form-group row">
-                                                                                <label class="col-sm-2 col-form-label">Upload File</label>
-                                                                                <div class="col-sm-10">
-                                                                                   
-                                                                        <input type="file" name="img" class="form-control">
-                                                                                </div>     
-                                                                                </div>   
-                                                                
-                                                                       <button type="submit" class="btn btn-primary" name="add" id="primary-popover-content" data-container="body" data-toggle="popover" title="Primary color states" data-placement="bottom" data-content="<div class='color-code'>
+
+	$query = "INSERT INTO products(title, img, price, description, info, dep_id) ";
+
+	$query .= "VALUES('{$title}', '{$img}', '{$price}', '{$description}', '{$info}', '{$id_dep}') ";
+
+    mysqli_query( $connection, $query);
+
+	echo "<p style='color: #0A77F4; font-size: 16px; padding-top: 10px;'>$title Added.</p>";
+
+
+}
+
+?>
+    <form method="post" enctype="multipart/form-data">
+    <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Product Name</label>
+        <div class="col-sm-10">
+            <input type="text" name="title" class="form-control">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Price</label>
+        <div class="col-sm-10">
+            <input type="text" name="price" class="form-control">
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Select Category</label>
+        <div class="col-sm-10">
+            <select name="category" class="form-control">
+
+				<?php
+
+				$query = "SELECT * FROM departments";
+
+				$select_all_query = mysqli_query( $connection, $query );
+
+				while ( $row = mysqli_fetch_array( $select_all_query ) ) {
+
+					$id    = $row['id'];
+					$title = $row['title'];
+
+					echo "<option value='$id'>$title</option>";
+
+				}
+
+
+				?>
+
+            </select>
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Description</label>
+        <div class="col-sm-10">
+            <textarea rows="5" cols="5" class="form-control" name="description"></textarea>
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Information</label>
+        <div class="col-sm-10">
+            <textarea rows="5" cols="5" name="info" class="form-control"></textarea>
+        </div>
+    </div>
+
+
+    <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Upload File</label>
+        <div class="col-sm-10">
+
+            <input type="file" name="img" class="form-control">
+        </div>
+    </div>
+
+    <button type="submit" class="btn btn-primary" name="add" id="primary-popover-content" data-container="body"
+            data-toggle="popover" title="Primary color states" data-placement="bottom" data-content="<div class='color-code'>
                                                                         <div class='row'>
                                                                           <div class='col-sm-6 col-xs-12'>
                                                                             <span class='block'>Normal</span>
@@ -162,8 +164,9 @@ echo "<option value='$id'>$title</option>";
                                                       </div>
                                                   </div>
 
-                                                  ">Primary button</button>
-                                                                
-                                                                   
-      
+                                                  ">Primary button
+    </button>
+
+
+
 <?php include "includes/footer.php" ?>
